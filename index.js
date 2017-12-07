3284fcf732153f7ea041ee35ba9f073c36d0b9b1
Nakajima Kazuki <JP22274@w022571708172m.local>
2017-12-07 20:22:27 +0900

    improving

--- index.js
+++ index.js
@@ -14,10 +14,37 @@ const line_config = {
 // Webサーバー設定
 server.listen(process.env.PORT || 3000);
 
+// APIコールのためのクライアントインスタンスを作成
+const bot = new line.Client(line_config);
 
 // -----------------------------------------------------------------------------
 // ルーター設定
 server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+    // 先行してLINE側にステータスコード200でレスポンスする。
     res.sendStatus(200);
-    console.log(req.body);
+
+    // すべてのイベント処理のプロミスを格納する配列。
+    let events_processed = [];
+
+    // イベントオブジェクトを順次処理。
+    req.body.events.map((event) => {
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            if (event.message.text == "こんにちは"){
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "これはこれは"
+                }));
+            }
+        }
+    });
+
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
 });
